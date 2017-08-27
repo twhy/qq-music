6210ad4edb7d31c7d8763d49c391756af1ea3cbb
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-27 22:23:14 +0800

    Tab

---
+++ scripts/tab.js
@@ -0,0 +1,22 @@
+document.addEventListener('click', function(event) {
+  
+  let target = event.target
+
+  if (target.dataset.role !== 'tab') return
+
+  [].forEach.call(target.parentElement.children, tab => {
+    tab.classList.remove('active')
+  })
+
+  target.classList.add('active')
+    
+  let content = document.querySelector(target.dataset.view)
+
+  if (content) {
+    [].forEach.call(content.parentElement.children, child => {
+      child.style.display = 'none'
+    })
+    content.style.display = 'block'
+  }
+
+})
