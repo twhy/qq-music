e0cc592e8eea65747ebeba4774383e59aae61f6a
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-30 23:20:20 +0800

    Lazyload

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,51 @@
 (function() {
 
-  let slider = new Slider({
-    el: document.querySelector('#slider'),
-    slides: [
-      { link: '#1', image: 'images/xiha.jpg' },
-      { link: '#2', image: 'images/xgs.jpg' },
-      { link: '#3', image: 'images/ljj.jpg' },
-      { link: '#4', image: 'images/xl.jpg' },
-      { link: '#5', image: 'images/joey.jpg' }
-    ]
-  })
+  fetch('/json/rec.json')
+    .then(res => res.json())
+    .then(render)
+
+  function render(json) {
+    renderSlider(json.data.slider)
+    renderRadios(json.data.radioList)
+    renderPlaylists(json.data.songList)
+    lazyload(document.querySelectorAll('.lazyload'))
+  }
+
+  function renderSlider(slides) {
+    slides = slides.map(slide => {
+      return { link: slide.linkUrl, image: slide.picUrl }
+    })
+    new Slider({
+      el: document.querySelector('#slider'),
+      slides
+    })
+  }
+
+  function renderRadios(radios) {
+    document.querySelector('.radios .list').innerHTML = radios.map(radio => 
+      `<div class="list-item">
+        <div class="list-media">
+          <img class="lazyload" data-src="${radio.picUrl}">
+          <span class="icon icon-play"></span>
+        </div>
+        <div class="list-detail">
+          <h3 class="list-title">${radio.Ftitle}</h3>
+        </div>
+      </div>`).join('')
+  }
+
+  function renderPlaylists(playlists) {
+    document.querySelector('.playlists .list').innerHTML = playlists.map(list =>
+      `<div class="list-item">
+        <div class="list-media">
+          <img class="lazyload" data-src="${list.picUrl}">
+          <span class="icon icon-play"></span>
+        </div>
+        <div class="list-detail">
+          <h3 class="list-title">${list.songListDesc}</h3>
+          <div class="list-text"></div>
+        </div>
+      </div>`).join('')
+  }
 
 })()
---
+++ scripts/lazyload.js
@@ -0,0 +1,64 @@
+function lazyload(imgs) {
+  imgs = Array.from(imgs)
+
+  if ('IntersectionObserver' in window) {
+    let observer = new IntersectionObserver(function(entries) {
+      console.log(entries)
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          loadImage(entry.target, function() {
+            observer.unobserve(entry.target)
+          })
+        }
+      })
+    }, { threshold: 0.01 })
+  
+    imgs.forEach(img => observer.observe(img))
+  } else {
+    let onscroll = throttle(function() {
+      if (imgs.length === 0) {
+        return window.removeEventListener('scroll', onscroll)
+      }
+      imgs = imgs.filter(img => img.classList.contains('lazyload'))
+      imgs.forEach(img => inViewport(img) && loadImage(img))
+    }, 300)
+  
+    window.addEventListener('scroll', onscroll)
+    window.dispatchEvent(new Event('scroll'))
+  }
+
+  function throttle(func, wait) {
+    let prev, timer
+    return function fn() {
+      let curr = Date.now()
+      let diff = curr - prev
+      if (!prev || diff >= wait) {
+        func()
+        prev = curr
+      } else if (diff < wait) {
+        clearTimeout(timer)
+        timer = setTimeout(fn, wait - diff)
+      }
+    }
+  }
+
+  function inViewport(img) {
+    let { top, left, right, bottom } = img.getBoundingClientRect()
+    let vpWidth = document.documentElement.clientWidth
+    let vpHeight = document.documentElement.clientHeight
+    return (
+      (top > 0 && top < vpHeight || bottom > 0 && bottom < vpHeight) &&
+      (left > 0 && left < vpWidth || right > 0 && right < vpWidth)
+    )
+  }
+
+  function loadImage(img, callback) {
+    let image = new Image()
+    image.src = img.dataset.src
+    image.onload = function() {
+      img.src = image.src
+      img.classList.remove('lazyload')
+      if (typeof callback === 'function') callback()
+    }
+  }
+}
