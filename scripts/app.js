53b29be8b99ae8a3cd32bda4b3e0c1f2b411f1ab
Tom Wang <tom.wanghaiyi@gmail.com>
2017-09-01 22:36:05 +0800

    Rank and Server API

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,26 @@
     .then(res => res.json())
     .then(render)
 
+  fetch('/json/rank.json')
+    .then(res => res.json())
+    .then(json => json.data.topList)
+    .then(renderTopList)
+
   function render(json) {
     renderSlider(json.data.slider)
     renderRadios(json.data.radioList)
     renderPlaylists(json.data.songList)
     lazyload()
   }
+
+  let $search = document.querySelector('#search')
+  $search.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      fetch(`https://qq-music-api.now.sh/search?keyword=${this.value}`)
+        .then(res => res.json())
+        .then(console.log)
+    }
+  })
 
   function renderSlider(slides) {
     slides = slides.map(slide => ({ link: slide.linkUrl, image: slide.picUrl }))
@@ -43,4 +57,30 @@
       </div>`).join('')
   }
 
+  function renderTopList(list) {
+    document.querySelector('#rank-view .toplist').innerHTML = list.map(item => 
+      `<li class="top-item">
+      <div class="top-item-media">
+        <a href="#">
+          <img class="lazyload" data-src="${item.picUrl}">
+        </a>
+      </div>
+      <div class="top-item-info">
+        <h3 class="top-item-title ellipsis">${item.topTitle}</h3>
+        <ul class="top-item-list">
+          ${songlist(item.songList)}
+        </ul>
+      </div>
+    </li>`).join('')
+
+    lazyload(document.querySelectorAll('#rank-view .toplist .lazyload'))
+
+    function songlist(songs) {
+      return songs.map((song, i) => 
+        `<li class="top-item-song">
+          <i class="song-index">${i + 1}</i>
+          <span class="song-name">${song.songname}</span>- ${song.singername}
+        </li>`).join('')
+    }
+  }
 })()
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const request = require('request-promise')
+const app = express()
+const PORT = process.env.PORT || 4000
+
+const HEADERS = {
+  'accept': 'application/json',
+  'authority': 'c.y.qq.com',
+  'origin': 'https://m.y.qq.com',
+  'referer': 'https://m.y.qq.com/',
+  'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+}
+
+app.get('/', async (req, res) => {
+  const url = `https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=${+ new Date()}`
+  try {
+    res.json(await request({
+      uri: url,
+      json: true,
+      headers: HEADERS
+    }))
+  } catch(e) {
+    res.json({ error: e.message })
+  }
+})
+
+app.get('/search', async (req, res) => {
+  const { keyword, page = 1 } = req.query
+  const url = `https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&w=${encodeURIComponent(keyword)}&zhidaqu=1&catZhida=1&t=0&flag=1&ie=utf-8&sem=1&aggr=0&perpage=20&n=20&p=${page}&remoteplace=txt.mqq.all&_=${+new Date()}`
+  try {
+    res.json(await request({
+      uri: url,
+      json: true,
+      headers: HEADERS
+    }))
+  } catch(e) {
+    res.json({ error: e.message })
+  }
+})
+
+app.get('/hotkey', async (req, res) => {
+  const url = `https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=${+new Date()}`
+  try {
+    res.json(await request({
+      uri: url,
+      json: true,
+      headers: HEADERS
+    }))
+  } catch(e) {
+    res.json({ error: e.message })
+  }
+})
+
+app.listen(PORT)
